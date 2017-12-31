9a7f6143e649db7435353008a4e404adc1a55480
a7567092@hotmail.com <a7567092@hotmail.com>
2017-12-31 22:03:15 +0800

    add recycle holder and adapter(unfinish)

---
+++ cnBetaPersonalVersion/Article.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace cnBetaPersonalVersion
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public DateTime PulishTime { get; set; }
+        public int ReadTimes { get; set; }
+        public string ID { get; set; }
+        public string URL { get; set; }
+        public string ImageURL { get; set; }
+    }
+    public class ArticleList
+    {
+        static Article[] articleList = {
+            new Article{Title="北京首条磁浮线路开始试运营",
+            Summary="12月30日，北京首条中低速磁浮轨道交通S1线开始试运营，标志着中低速磁浮交通技术工程化研发成果在北京成功落地。据了解，S1线连接石景山区和门头沟区，是北京西部居民出行的重要交通线路，线路由苹果园站（暂缓开通）、金安桥站、四道桥站、桥户营站、上岸站、栗园庄站、小园站和石厂站八座车站组成。",
+            Type="科技",
+            PulishTime=new DateTime(2017,12,31),
+            ReadTimes=910,
+            ImageURL="https://static.cnbetacdn.com/thumb/mini/article/2017/1231/fc985fcd2610bf1.jpg"}
+        };
+
+        public Article this[int i]
+        {
+            get { return articleList[i]; }
+        }
+
+        public int ArticleNum
+        {
+            get { return articleList.Length; }
+        }
+    }
+}
---
+++ cnBetaPersonalVersion/RecyclerViewHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using Java.Net;
+
+namespace cnBetaPersonalVersion
+{
+    public class ArticleHolder:RecyclerView.ViewHolder
+    {
+        public ImageView Image { get; private set; }
+        public TextView Title { get; private set; }
+        public TextView Summary { get; private set; }
+        public TextView Other { get; private set; }
+
+        public ArticleHolder(View itemview,Action<int>listener):base(itemview)
+        {
+            Image = itemview.FindViewById<ImageView>( Resource.Id.imageView);
+            Title = itemview.FindViewById<TextView>(Resource.Id.textViewTitle);
+            Summary = itemview.FindViewById<TextView>(Resource.Id.textViewSummary);
+            Other = itemview.FindViewById<TextView>(Resource.Id.textViewOther);
+            itemview.Click += (sender, e) => listener(base.LayoutPosition);
+        }
+    }
+
+    public class ArticleAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<int> ItemClick;
+        void OnClick(int position)
+        {
+            if (ItemClick != null)
+                ItemClick(this, position);
+        }
+
+        public ArticleList articleList;
+        public ArticleAdapter(ArticleList atl)
+        {
+            articleList = atl;
+        }
+        public override int ItemCount => throw new NotImplementedException();
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            ArticleHolder ah = holder as ArticleHolder;
+            //ah.Image.setim.SetImageURI(new Android.Net.Uri.Builder(articleList[position].ImageURL));
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.ArticleCardView, parent, false);
+            ArticleHolder ah = new ArticleHolder(itemView, OnClick);
+            return ah;
+        }
+    }
+}
